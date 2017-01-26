326041f2b4ec2083f195b957a6dd421a25b13e1e
Marlene Uwamahoro <uwamahoromarlene@gmail.com>
2017-01-26 10:35:02 +0200

    Added model methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,6 +19,8 @@ class ProductsController < ApplicationController
     description = params[:description]
     product = Product.new({name: name, price: price, image: image, description: description})
     product.save
+    flash[:success] = "Your product is created"
+    redirect_to "/products/#{product.id}"
   end
 
   def edit
@@ -26,11 +28,20 @@ class ProductsController < ApplicationController
   end
 
   def update
+    product = Product.find_by(id: params[:id])
+    product.name = params[:name]
+    product.price = params[:price]
+    product.image = params[:image]
+    product.description = params[:description]
+    product.save
+    flash[:success] = "Your product is updated"
+    redirect_to "/products/#{product.id}"
+  end
+
+  def destroy
     @product = Product.find_by(id: params[:id])
-    @product.name = params[:name]
-    @product.price = params[:price]
-    @product.image = params[:image]
-    @product.description = params[:description]
-    @product.save
+    @product.destroy
+    flash[:success] = "Your product is destroyed"
+    redirect_to "/products"
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ApplicationRecord
+
+  def friendly_created_at
+    created_at.strftime("%B %e, %Y")
+  end
+
+  def calculate_tax
+    return (price * 9)/100
+  end
+
 end
